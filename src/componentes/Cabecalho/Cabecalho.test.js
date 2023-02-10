2dd35f11a762144d4bc2d821d670de87deab5f98
NeiltonSeguins <seguins.neilton@gmailcom>
2023-02-10 14:06:47 -0300

    " Atualização da branch "

--- src/componentes/Cabecalho/Cabecalho.test.js
+++
@@ -1,9 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-import { BrowserRouter } from 'react-router-dom';
-import Cabecalho from './index';
-
-test.skip('Deve renderizar o nome do usuário logado', async () => {
-  render(<Cabecalho />, { wrapper: BrowserRouter });
-  const nomeUsuario = await screen.findByText('Neilton Seguins');
-  expect(nomeUsuario).toBeInTheDocument();
-});
